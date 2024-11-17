b786d013fa1da828fb82c73d7afd6f3c3874bf0e
n33naa <NAEEMKHGG@gmail.com>
2024-11-17 22:41:51 +0300

    Add files via upload

---
+++ n3n3/g.js
@@ -0,0 +1,17 @@
+// script.js
+
+//document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // منع إرسال النموذج بشكل افتراضي
+
+   // const email = document.getElementById('email').value;
+  //  const password = document.getElementById('password').value;
+
+    // تحقق من البيانات (هذا مثال بسيط، يمكنك توسيعه ليتحقق من البيانات الحقيقية)
+   // if (email === "" || password === "") {
+        alert("يرجى ملء جميع الحقول.");
+ //   } else {
+    //   alert("تسجيل الدخول ناجح!");
+        // يمكنك إضافة التوجيه إلى صفحة أخرى هنا باستخدام:
+        // window.location.href = "home.html";
+ //   }
+//}//);
